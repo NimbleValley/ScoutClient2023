5316852901bc06258b1472508c5bdaf2adfdbf4e
Mason McManus <97319135+NimbleValley@users.noreply.github.com>
2023-10-19 22:46:52 -0500

    Waytt DLC wanted more images so I added more

--- post.js
+++ post.js
@@ -9,7 +9,7 @@ const autoChargeOutput = document.getElementById("auto-charge");
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbzk1w9sYekFbdUre5kaGVnjtnqSVr9PpHrCgfFvb261u_gFfUMPgPSh1UDR3WAFzFxbpw/exec";
 
-const respectInducingImages = ["img/sherril5.jpg", "img/trompmil24.jpg", "img/sherril.jpg", "img/snow.jpg", "img/sherrilwatching.jpg", "img/joseph1.jpg"];
+const respectInducingImages = ["img/sherril5.jpg", "img/trompmil24.jpg", "img/sherril.jpg", "img/snow.jpg", "img/sherrilwatching.jpg", "img/joseph1.jpg", "img/zachary01.jpg", "img/waytt.jpg", "img/bendi.jpg"];
 
 document.addEventListener("keypress", function () {
     //document.getElementById("respect-your-elders-image").src = respectInducingImages[Math.round(Math.random() * (respectInducingImages.length-1))];
